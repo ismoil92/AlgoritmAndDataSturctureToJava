c48f2423e7ef179ce03abc61022192bc1e857160
Ismoil Muhammadiev <ism0il.muhammadiev92@gmail.com>
2023-06-03 12:22:05 +0500

    Выполнена 4-ое задание. Реализовать связной Хэш таблицу со своими методами.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashTable hashTable = new HashTable();
+        for (int i = 0; i < 32; i++) {
+            hashTable.put( i + 1, i);
+        }
+        System.out.println(hashTable.get(30));
+        hashTable.replays(30, 2318);
+
+        System.out.println(hashTable.get(30));
+        hashTable.remove(30);
+        System.out.println(hashTable.get(30));
     }
 }
